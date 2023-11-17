37e086532b332f00609310c685cc50e98725944b
MNTSheridan <146022584+MNTSheridan@users.noreply.github.com>
2023-11-17 20:30:21 +0000

    Part 1 User interaction methods implemented.

--- ApplicationFolder/Applicationfile.py
+++ ApplicationFolder/Applicationfile.py
@@ -10,7 +10,59 @@ class Bank:
         self._bankName = bankName
 
 class Application:
-    pass
+    def showMainMenu():
+        while True:
+            menuMessage = '''
+            Banking Application Menu:\n
+            1. Select Account.\n
+            2. Open Account.\n
+            3. Exit.\n
+            '''
+            try:
+                choice = int(input(menuMessage))
+            except ValueError:
+                print("Try again")
+                choice = int(input(menuMessage))
+            if choice == 1:
+                pass
+            elif choice == 2:
+                pass
+            elif choice == 3:
+                pass
+            else:
+                pass
+
+    def showAccountMenu():
+        while True:
+            menuMessage = '''
+            Banking Application Account Menu:\n
+            1. Check Balance:\n
+            2. Deposit.\n
+            3. Withdraw.\n
+            4. Exit Account.
+            '''
+            try:
+                choice = int(input(menuMessage))
+            except ValueError:
+                print("Try again")
+                choice = int(input(menuMessage))
+            if choice == 1:
+                pass
+            elif choice == 2:
+                pass
+            elif choice == 3:
+                pass
+            elif choice == 4:
+                print("You have chosen to exit the account, thank you for using our banking system.")
+                break
+            else:
+                pass
+    
+    def run():
+        pass
+
+    run()
+
 
 class SavingsAccount(Account):
     pass
